d9483366a04816fcd9b2cabf43b7c132ea0b3cd3
Lindapig <lindaz888666@gmail.com>
2024-02-23 17:17:25 -0500

    JavaScript and React.js A3

---
+++ src/Nav.tsx
@@ -0,0 +1,13 @@
+import { Link, useLocation } from "react-router-dom";
+function Nav() {
+    const { pathname } = useLocation();
+
+    return (
+        <nav className="nav nav-pills mt-2">
+            <Link className={`nav-link ${pathname.includes("Labs") ? "active" : ""}`} to="Labs/a3">A3</Link>
+            <Link className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`} to="/Kanbas">Kanbas</Link>
+            <Link className={`nav-link ${pathname.includes("hello") ? "active" : ""}`} to="/hello">Hello</Link>
+        </nav >
+    );
+}
+export default Nav;
